5a8e90ebf3bb63f66294c0aeb68196f09101f8df
Wissididom <30803034+Wissididom@users.noreply.github.com>
2026-06-14 12:03:28 +0200

    Replace information tag with console log

--- renderer.js
+++ renderer.js
@@ -4,8 +4,7 @@ const func = async () => {
 };
 func();
 
-const information = document.getElementById('info')
-information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
+console.log(`This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`);
 
 let totalSeconds = 30 * 60; // 30 minutes
 let remaining = totalSeconds;
